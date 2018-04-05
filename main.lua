897b79daee7f7230c954941a01eb7de7964945cf
Lucas Black <hmnotsocool@gmail.com>
2018-04-05 07:11:28 -0700

    polish

--- main.lua
+++ main.lua
@@ -1,24 +1,40 @@
 local Camera = require "camera"
 local pipes = {}
+local JumpSound = love.audio.newSource("jump.wav")
+JumpSound:setVolume(.3)
+local DiedSound = love.audio.newSource("died.wav")
+DiedSound:setVolume(.3)
+local GameState = "play"
 
 local pipeDelta = 300
 
 -- print (love.graphics.getHeight(), 6 * love.graphics.getHeight / 8)
 
+function factorial(n)
+	if n == 0 then n = 1 else
+		for i=2, n-1 do
+			n=n*i
+		end
+	end
+	return n
+end
+
 for i = 1, 100 do
+	local fac = factorial(i)
 	local width = 64
-	local gap = love.graphics.getHeight() / 4
-	local height = love.math.random(1, 6) * love.graphics.getHeight() / 8
+	local xGap = 64 * fac / (1.2 * fac)
+	local gap = love.graphics.getHeight() / 3.8
+	local height = love.math.random(1, 6) * love.graphics.getHeight() / 12
 	
 	local temp1 = {
-		x = i * pipeDelta + width,
+		x = i * pipeDelta + width + xGap,
 		y = 0,
 		width = width,
 		height = height
 	}
 
 	local temp2 = {
-		x = i * pipeDelta + width,
+		x = i * pipeDelta + width + xGap,
 		y = height + gap,
 		width = width,
 		height = love.graphics.getHeight() - height + gap 
@@ -33,41 +49,84 @@ function drawPipes()
 
 	for i = 1, #pipes do
 		local pipe = pipes[i]
-		love.graphics.rectangle("fill", pipe.x, pipe.y, pipe.width, pipe.height)
+		if pipe.x <= cam.x + love.graphics.getWidth() / 2
+		or pipe.x >= cam.x - love.graphics.getWidth() / 2 then
+			love.graphics.rectangle("fill", pipe.x, pipe.y, pipe.width, pipe.height)
+		end
 	end
 end
 
+-- empirical constants
+local birdSpeed = 145
 local bird = {
 	x = 0,
 	y = 0,
-	vx = 100,
+	vx = birdSpeed,
 	vy = 0,
-	width = 32,
-	height = 32,
-	jumpVel = -335,
-	gravity = 600,
+	width = 24,
+	height = 24,
+	jumpVel = -420,
+	gravity = 790,
 	score = 0
 }
+
+function bird:playSound(source)
+	if source:isPlaying() then
+		source:stop()
+		source:play()
+	else
+		source:play()
+	end
+end
 
 function bird:jump(key)
 	if key == "space" then
 		self.vy = self.jumpVel
+		self.vx = self.vx + -1 * self.jumpVel * love.timer.getDelta()
+		self:playSound(JumpSound)
 	end
 end
 
 function bird:update(dt)
+	-- move to the right at a linear rate
 	self.x = self.x + self.vx * dt
 
+	-- apply gravity
 	if self.vy < self.gravity then
 		self.vy = self.vy + self.gravity * dt
 	else
 		self.vy = self.gravity
 	end
 
+	-- move upward or downward at a quadratic rate
 	self.y = self.y + self.vy * dt
 
-	if self.y < 0 or self.y + self.height > love.graphics.getHeight() then
-		love.event.quit("restart")
+	-- limit upward movement to the top of the window
+	if self.y < 0 then
+		self.y = 0
+		self.vy = 0
+	end
+
+	-- detect collision with any pipe
+	local hitPipe, pipe, index
+	index = self.score ~= 0 and self.score or 1
+	for i = index, #pipes do
+		hitPipe, pipe = false, pipes[i]
+		if pipe.x < self.x + self.width
+		and pipe.x + pipe.width > self.x
+		and pipe.y < self.y + self.height
+		and pipe.y + pipe.height > self.y then
+			hitPipe = true
+			break
+		end
+	end
+
+
+	-- check for collisions or going below the screen
+	if self.y + self.height > love.graphics.getHeight() or hitPipe then
+		-- die
+		GameState = "died"
+		self:playSound(DiedSound)
 	end
 
 	self:updateScore()
@@ -91,8 +150,11 @@ end
 cam = Camera(bird.x, bird.y)
 
 function love.update(dt)
-	bird:update(dt)
-	cam:lookAt(bird.x, love.graphics.getHeight()/2)
+	if GameState == "play" then
+		bird:update(dt)
+		cam:lockX(bird.x, Camera.smooth.damped(10))
+		cam:lockY(love.graphics.getHeight() / 2)
+	end
 end
 
 function love.draw()
@@ -108,9 +170,13 @@ function love.draw()
 end
 
 function love.keypressed(key)
-	bird:jump(key)
+	if GameState == "play" then
+		bird:jump(key)
+	end
 
 	if key == "escape" then
 		love.event.quit()
+	elseif key == "r" then
+		love.event.quit("restart")
 	end
 end
